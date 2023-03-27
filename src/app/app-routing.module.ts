b3e69912207d7e0d2e11f62b974a8026c1667402
Agnes Kaswii <agneskaswii700@gmail.com>
2023-03-27 10:52:28 +0300

    adding routing to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+
+
+
+const routes: Routes = [
+  { path: 'about', component: AboutusComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'contact', component: ContactComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
