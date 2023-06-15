1c51108aa467adc54df727b3301c8af830be9fdd
Agnes Kaswii <agneskaswii700@gmail.com>
2023-06-15 10:42:12 +0300

    redoing the carousel and styling it

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css']
 })
-export class AboutusComponent {
+export class AboutusComponent  implements AfterViewInit{
+  ngAfterViewInit(): void {
+    document.addEventListener("DOMContentLoaded", function() {
+      const carouselItems = document.querySelectorAll(".carousel-item");
+      carouselItems.forEach(function(item, index) {
+        setTimeout(function() {
+          item.classList.remove("carousel-item-initial");
+        }, index * 1000);
+      });
+    });
+  }
 
-  images = [
-    {img :"/assets/images/_LK23475.jpg"},
-    {img :"/assets/images/230328_KDEN_PHT163.jpg"},
-    {img :"/assets/images/230328_Ldena_PHT068.jpg"},
-    {img:"/assets/images/agrf pix.jpeg" },
-  
-  ];
 
 }
